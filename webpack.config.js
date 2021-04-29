b67a1e4a8108e4a9edddb37c02106a735b7a13aa
Dhrubo <dhrubo.dh@gmail.com>
2021-04-28 21:57:14 -0400

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './lib/components/Index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }],
+    },
+};
